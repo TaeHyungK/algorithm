b7ae885c9895848a8802b278ceb5c82bbee67258
taehyungK <xogud3125@naver.com>
2019-10-17 23:39:03 +0900

    프로그래머스 > 평균 구하기

--- README.md
+++ README.md
@@ -74,3 +74,6 @@
 
 - [프로그래머스 > 콜라츠 추측](https://programmers.co.kr/learn/courses/30/lessons/12943)
   - https://github.com/TaeHyungK/algorithm/blob/master/PGLevel1Q22
+  
+- [프로그래머스 > 평균 구하기](https://programmers.co.kr/learn/courses/30/lessons/12944)
+  - https://github.com/TaeHyungK/algorithm/blob/master/PGLevel1Q23
---
+++ src/programmers/PGLevel1Q23.java
@@ -0,0 +1,20 @@
+package programmers;
+
+/**
+ * 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+ */
+public class PGLevel1Q23 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        System.out.println(solution(arr));
+    }
+
+    public static double solution(int[] arr) {
+        int sum = 0;
+        for (int value : arr) {
+            sum += value;
+        }
+
+        return (double) sum / arr.length;
+    }
+}
