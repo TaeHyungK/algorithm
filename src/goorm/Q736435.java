05c055531dd059e7563196c890e6c2a5d484f0cf
taehyungK <xogud3125@naver.com>
2020-01-09 22:43:53 +0900

    [알고리즘][구름] Q736435 미해결

---
+++ src/goorm/Q736435.java
@@ -0,0 +1,24 @@
+package goorm;
+
+import java.io.*;
+
+/**
+ * 환상의 조합 (★2)
+ *
+ * https://edu.goorm.io/learn/lecture/15551/%EB%A7%A4%EC%A3%BC-%EB%B0%B0%EC%86%A1%EB%B0%9B%EB%8A%94-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%ED%94%84%EB%A6%AC%EB%AF%B8%EC%97%84-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EC%9C%84%ED%81%B4%EB%A6%AC-%EB%B9%84%ED%83%80%EC%95%8C%EA%B3%A0-%EC%8B%9C%EC%A6%8C2/lesson/736435/09%EC%9B%94-1%EC%A3%BC%EC%B0%A8-%ED%99%98%EC%83%81%EC%9D%98-%EC%A1%B0%ED%95%A9-2
+ *
+ */
+public class Q736435 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        String[] nsString = input.split(" ");
+        int n = Integer.parseInt(nsString[0]);
+        int s = Integer.parseInt(nsString[1]);
+
+        input = br.readLine();
+        String[] stats = input.split(" ");
+
+        // TODO 문제 이해하고 마저 풀어보자...
+    }
+}
