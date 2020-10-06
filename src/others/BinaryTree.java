351b75a23d89f7c00355f30f0aff1fd539d00e9a
TaeHyungK <xogud3125@naver.com>
2020-10-06 23:35:52 +0900

    [Add] others > 이진트리 구현해보기

---
+++ src/others/BinaryTree.java
@@ -0,0 +1,38 @@
+package others;
+
+/**
+ * 이진 Tree 구현해보기.
+ */
+public class BinaryTree {
+    private Object data;
+    private BinaryTree left;
+    private BinaryTree right;
+
+    public BinaryTree(Object data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
+    public void addLeft(BinaryTree node) {
+        if (left != null) left = null;
+        left = node;
+    }
+
+    public void addRight(BinaryTree node) {
+        if (right != null) right = null;
+        right = node;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public BinaryTree getLeft() {
+        return left;
+    }
+
+    public BinaryTree getRight() {
+        return right;
+    }
+}
